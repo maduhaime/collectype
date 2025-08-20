4bd7ff1af230230bc05cb983f6e058c135b67c24
Marc-Antoine Duhaime <ma@duhaime.ca>
2025-08-20 17:52:21 -0400

    better doc

--- src/interfaces/Collectable.ts
+++ src/interfaces/Collectable.ts
@@ -1,14 +1,24 @@
 /**
  * Represents a generic structure that contains items and allows access to them.
+ *
+ * @template T The type of items contained in the collection.
+ *
+ * @example
+ * class MyCollection implements Collectable<string> {
+ *   get items() { return ['a', 'b', 'c']; }
+ *   get count() { return this.items.length; }
+ * }
  */
 export interface Collectable<T> {
   /**
-   * Returns the array of items.
+   * Gets the array of items contained in the collection.
+   * @returns {T[]} The items in the collection.
    */
   get items(): T[];
 
   /**
-   * Returns the number of items.
+   * Gets the number of items in the collection.
+   * @returns {number} The count of items.
    */
   get count(): number;
 }
