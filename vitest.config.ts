26f45e39c971752be56e60cc1a0293710344bae3
Marc-Antoine Duhaime <ma@duhaime.ca>
2025-08-30 11:44:15 -0400

    Drop support for alias
    Reduce the risk off error when used as a package

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,6 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   test: {
     environment: 'node',
     coverage: {
